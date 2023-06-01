6d1a2f91049da7f80ef8a88a86c415eb9c768825
spencerhite <shite001@odu.edu>
2023-06-01 08:32:55 -0400

    Created basis for the program

---
+++ twoSum.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Spencer on 6/1/23.
+//
+
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution{
+public:
+    vector<int> twoSum(vector<int>& nums, int target){
+
+
+    }
+};
+
+int main(int twoSum){
+
+    vector<int> nums = {2,7,11,15};
+
+
+    int target = 9;
+}
+
